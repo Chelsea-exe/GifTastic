f54052e496cc7b6606442328264df1b8d53c068e
Chelsea-exe <cjackson145@student.gsu.edu>
2020-02-03 18:10:02 -0500

    added new javascript (js2) to html so buttons would properly load up on buttons div and connect to giphy api,and load onto gifsappear div. didn't get the chance to add the [freeze] effect of the gifs

--- javascript2.js
+++ javascript2.js
@@ -1,5 +1,114 @@
 var women = ["Beyonce", "Michelle Obama", "Rihanna", "Alexandria Ocasio-Cortez"];
 
+// function showGifs() {
+//     var women = $(this).attr("data-name");
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         women + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         var gifs = $("<div>");
+//         var rating = results[i].rating;
+//         var p = $("<p>").text("Rating: " + rating);
+//         var womenImage = $("<img>")
+//     });
+// }
+
+// function showGifs(womenGifs) {
+//     console.log(womenGifs);
+// var women = $(this).attr("data-name");
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         women + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response);
+//         for (var i = 0; i < response.data.length; i++) {
+//             var gif = $("<div>");
+
+//                 var rating = results[i].rating;
+
+//                 var p = $("<p>").text("Rating: " + rating);
+
+//                 var womenImage = $("<img>");
+//                 womenImage.attr("src", results[i].images.fixed_height.url);
+
+//                 gif.append(p);
+//                 gif.append(womenImage);
+
+//                 $("#gifsappear").prepend(gif);
+//         }
+//     });
+// }
+
+// function addButons() {
+//     $('#display-buttons').empty();
+//     for (var i = 0; i < women.length; i++) {
+//         var womenButtons = $("<button>");
+//         womenButtons.addClass("name-btn");
+//         womenButtons.attr("data-name", women[i]);
+//         womenButtons.text(women[i]);
+//         $("#display-buttons").append(womenButtons);
+//     }
+// };
+
+// $("#add-woman").on("click", function(event) {
+//    event.preventDefault();
+//    var powerful = $("#woman-input").val().trim();
+//    women.push(powerful);
+//    addButons();
+// });
+// addButons();
+
+// $(document).on("click", ".name-btn", function() {
+//     var womenGifs = $(this).data("name");
+//     showGifs(womenGifs);
+// });
+
+// addButons();
+
+
+// $("button").on("click", function showGifs() {
+//     var women = $(this).attr("data-name");
+
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//         women + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     })
+    
+//     .then(function(response) {
+//         console.log(response);
+//         var results = response.data;
+
+//         for (var i = 0; i < results.length; i++) {
+
+//             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+//                 var gif = $("<div>");
+
+//                 var rating = results[i].rating;
+
+//                 var p = $("<p>").text("Rating: " + rating);
+
+//                 var image = $("<img>");
+
+//                 image.attr("src", results[i].images.fixed_height.url);
+
+//                 gif.append(p);
+//                 gif.append(image);
+
+//                 $("#gifsappear").prepend(gif);
+//             }
+//         }
+//     });
+// });
+
+
+// $(document).on("click", ".name-btn", showGifs);
 function renderButtons() {
     $('#display-buttons').empty();
     for (var i = 0; i < women.length; i++) {
@@ -22,12 +131,14 @@ $("#add-woman").on("click", function(event) {
     var powerful = $("#woman-input").val().trim();
     women.push(powerful);
 
-    renderButtons();
+    renderButtons(powerful);
 });
 renderButtons();
-$("button").on("click", function() {
-    var women = $(this).attr("data-name");
 
+$(document).on("click",".women", function(data) {
+    console.log(data)
+    var women = $(this).attr("data-name");
+    console.log(women)
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         women + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
     $.ajax({
@@ -52,6 +163,13 @@ $("button").on("click", function() {
                 var image = $("<img>");
 
                 image.attr("src", results[i].images.fixed_height.url);
+                // console.log(powerful)
+                console.log(women)
+                if (women === ""){
+                    image.attr("data-name", powerful)
+                } else {
+                    image.attr("data-name", women)
+                }
 
                 gif.append(p);
                 gif.append(image);
